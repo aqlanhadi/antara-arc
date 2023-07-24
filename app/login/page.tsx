fe86797f8e1568543e4e846f23bef2662b012d7c
Aqlan Nor Azman <aqlanhadi@gmail.com>
2023-07-24 19:51:16 +0800

    added icons

---
+++ app/login/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div className='flex'>
+        <div className='flex flex-col'>
+            <p>Create an account</p>
+        </div>
+        <div>
+            <p>Sign in</p>
+        </div>
+    </div>
+  )
+}
+
+export default Login
